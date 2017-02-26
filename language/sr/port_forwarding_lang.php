f7af1e804113ef4b5d8af5ca25f042ad464291b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:53:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Лука Просле апликација омогућава да се омогући приступ системима на локалној мрежи са Интернета.';
+$lang['port_forwarding_app_name'] = 'порт форвардинг';
+$lang['port_forwarding_feature_is_not_supported'] = 'Функција није подржана.';
+$lang['port_forwarding_port_forwarding'] = 'порт форвардинг';
+$lang['port_forwarding_port_range_warning'] = 'Порт опсега морају бити одређене помоћу једног почетак порта (нпр 15000) у једном циљу порт (нпр 15999).';
