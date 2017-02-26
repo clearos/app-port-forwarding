a89c233d6c29750498fa0b962172bcba8f90a7d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The Port Mikawa app sa ya yiwu don ba da damar samun tsarin on your gida na cibiyar sadarwa daga Intanit.';
+$lang['port_forwarding_app_name'] = 'Port Mikawa';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature ba da goyan.';
+$lang['port_forwarding_port_forwarding'] = 'Port Mikawa';
+$lang['port_forwarding_port_range_warning'] = 'Port jeri dole ne a kayyade amfani da tashar jiragen ruwa guda tashi (misali 15000) zuwa guda tashar jiragen ruwa karshen (misali 15999).';
