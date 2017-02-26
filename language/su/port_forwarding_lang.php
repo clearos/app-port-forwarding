1877565b15accff9e62f44cb31e6be8c6464c07d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:54:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Nu Port Neraskeun app ngamungkinkeun pikeun ngidinan aksés ka sistem dina jaringan lokal Anjeun ti internét.';
+$lang['port_forwarding_app_name'] = 'port Neraskeun';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature teu dirojong.';
+$lang['port_forwarding_port_forwarding'] = 'port Neraskeun';
+$lang['port_forwarding_port_range_warning'] = 'Port Bulan kudu husus maké port mimiti single (misalna 15000) ka port single ahir (misalna 15999).';
