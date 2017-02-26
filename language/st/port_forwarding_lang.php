864b8fcec48d6d4363ea480702e57fea454d813c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:53:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The app e Port fetisetse etsa hore ho ka khoneha ho lumella mekhoa ea ho fumana litsamaiso tsa leqhubu tsa hao tsa lehae tsoang Inthaneteng.';
+$lang['port_forwarding_app_name'] = 'Port fetisetse';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature e sa tšehetsoa.';
+$lang['port_forwarding_port_forwarding'] = 'Port fetisetse';
+$lang['port_forwarding_port_range_warning'] = 'mekoloko Port lokela ho boletsweng sebelisa e le &#39;ngoe-kepeng qale (mohlala 15000) to ea se nang molekane o koung ea qetellong (mohlala 15999).';
