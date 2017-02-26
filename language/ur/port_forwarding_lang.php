3abecd27d748874328d9551d714d01d37da712c3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:55:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'پورٹ فارورڈنگ اپلی کیشن یہ ممکن انٹرنیٹ سے آپ کے مقامی نیٹ ورک پر نظام تک رسائی کی اجازت دینے کے لئے بناتا ہے.';
+$lang['port_forwarding_app_name'] = 'پورٹ فارورڈنگ';
+$lang['port_forwarding_feature_is_not_supported'] = 'نمایاں کریں کی سہولت نہیں ہے.';
+$lang['port_forwarding_port_forwarding'] = 'پورٹ فارورڈنگ';
+$lang['port_forwarding_port_range_warning'] = 'پورٹ کی حدود (مثلا 15000) ایک واحد بندرگاہ اختتام (مثلا 15999) پر ایک واحد بندرگاہ آغاز کا استعمال کرتے ہوئے وضاحت ہونا ضروری ہے.';
