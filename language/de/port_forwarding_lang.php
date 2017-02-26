c17c85df418059f985d1c5d5da1e9e752f92fc53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Die Port Forwarding App macht es möglich, den Zugriff auf Systeme in Ihrem lokalen Netzwerk aus dem Internet zu ermöglichen.';
+$lang['port_forwarding_app_name'] = 'Port-Weiterleitung';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funktion wird nicht unterstützt.';
+$lang['port_forwarding_port_forwarding'] = 'Port-Weiterleitung';
+$lang['port_forwarding_port_range_warning'] = 'Portbereiche müssen einen einzelnen Port Start (zB 15000) mit einem einzigen Port Ende angegeben werden (zB 15999).';
