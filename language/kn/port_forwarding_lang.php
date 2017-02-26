daac175f7504853d6c0bc5f447518fac32aec467
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'ಪೋರ್ಟ್ ಫಾರ್ವರ್ಡ್ ಅಪ್ಲಿಕೇಶನ್ ಇದು ಸಾಧ್ಯ ಇಂಟರ್ನೆಟ್ ನಿಮ್ಮ ಸ್ಥಳೀಯ ಜಾಲದಲ್ಲಿ ಸಿಸ್ಟಮ್ಗಳಿಗೆ ಪ್ರವೇಶವನ್ನು ಅನುಮತಿಸುತ್ತದೆ ಮಾಡುತ್ತದೆ.';
+$lang['port_forwarding_app_name'] = 'ಪೋರ್ಟ್ ಫಾರ್ವರ್ಡ್';
+$lang['port_forwarding_feature_is_not_supported'] = 'ವೈಶಿಷ್ಟ್ಯ ಬೆಂಬಲಿಸುವುದಿಲ್ಲ.';
+$lang['port_forwarding_port_forwarding'] = 'ಪೋರ್ಟ್ ಫಾರ್ವರ್ಡ್';
+$lang['port_forwarding_port_range_warning'] = 'ಪೋರ್ಟ್ ವ್ಯಾಪ್ತಿಯ ನಡೆಸುವ ಸಿಂಗಲ್ ಪೋರ್ಟ್ ಅನ್ನು ಉಪಯೋಗಿಸಿ ಸೂಚಿಸಬೇಕು (ಉದಾ 15000) ಒಂದು ಬಂದರು ಕೊನೆಯಲ್ಲಿ (ಉದಾ 15999) ಗೆ.';
