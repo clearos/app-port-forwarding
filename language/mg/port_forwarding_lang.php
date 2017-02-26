e9d7dd8428ffcf270355c565730cc81dd45bcf2c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Ny Port nanatitra fampiharana dia ahafahana mamela ny fidirana amin&#39;ny rafitra eo amin&#39;ny tambajotra an-toerana avy ao amin&#39;ny Internet.';
+$lang['port_forwarding_app_name'] = 'Port nanatitra';
+$lang['port_forwarding_feature_is_not_supported'] = 'Endri-javatra dia tsy manohana.';
+$lang['port_forwarding_port_forwarding'] = 'Port nanatitra';
+$lang['port_forwarding_port_range_warning'] = 'Seranan-tsambo dia tsy maintsy mazava tandavan mampiasa seranan-tsambo iray monja nanomboka (oh: 15000) ny seranan-tsambo iray farany (ohatra: 15999).';
