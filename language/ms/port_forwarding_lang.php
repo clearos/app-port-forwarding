9382e370265e526b9ee847eb3da9307006291080
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Aplikasi Port Forwarding membolehkan untuk membolehkan akses kepada sistem pada rangkaian tempatan anda dari Internet.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Ciri tidak disokong.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'julat Port mesti dinyatakan menggunakan permulaan port tunggal (contohnya 15000) ke akhir port tunggal (contohnya 15999).';
