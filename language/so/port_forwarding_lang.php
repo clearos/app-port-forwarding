ade8b95e7f33168dc2c16ecbeec36f6447d0ba34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:53:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The app Port gudbinta waxay suuro gelineysaa in ay u oggolaadaan helitaanka nidaamka on your network maxaliga ah ka internetka.';
+$lang['port_forwarding_app_name'] = 'Port gudbinta';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature aan la taageeray.';
+$lang['port_forwarding_port_forwarding'] = 'Port gudbinta';
+$lang['port_forwarding_port_range_warning'] = 'safafka Port waa in la cayimay isticmaalaya hal bilowday dekedda (tusaale 15000) si ay hal dhamaadka dekedda (tusaale 15999).';
