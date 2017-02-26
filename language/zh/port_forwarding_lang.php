121c18cd4948e89a5ffbae00ca4c7f11c99b84fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:46:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = '端口转发应用程序，使得它可以允许从互联网本地网络上对系统的访问。';
+$lang['port_forwarding_app_name'] = '转发端口';
+$lang['port_forwarding_feature_is_not_supported'] = '不支持的功能。';
+$lang['port_forwarding_port_forwarding'] = '转发端口';
+$lang['port_forwarding_port_range_warning'] = '端口范围必须用一个端口启动（如15000）被指定为单口端（如：15999）。';
