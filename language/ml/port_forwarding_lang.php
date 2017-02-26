c32e62c35c33fe46f473d2887656e0be188e9b4b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'പോർട്ട് കൈമാറൽ അപ്ലിക്കേഷൻ ഇന്റർനെറ്റിൽ നിന്നും നിങ്ങളുടെ പ്രാദേശിക നെറ്റ്വർക്കിൽ സിസ്റ്റങ്ങളിലേക്കുമുള്ള പ്രവേശനം അനുവദിക്കാൻ സാധ്യമാക്കുന്നു.';
+$lang['port_forwarding_app_name'] = 'പോർട്ട് കൈമാറൽ';
+$lang['port_forwarding_feature_is_not_supported'] = 'ഫീച്ചർ പിന്തുണയ്ക്കുന്നില്ല.';
+$lang['port_forwarding_port_forwarding'] = 'പോർട്ട് കൈമാറൽ';
+$lang['port_forwarding_port_range_warning'] = 'പോര്ട് ശ്രേണികൾ ഒരൊറ്റ പോർട്ട് അവസാനം (ഉദാ 15999) ലേക്ക് ഒരൊറ്റ പോർട്ട് തുടക്കം (ഉദാ 15000) ഉപയോഗിച്ച് വ്യക്തമാക്കണം.';
