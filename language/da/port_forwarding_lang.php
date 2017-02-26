ef52e3a9fb992b6eb5e4154d9a0273406fe6f158
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:47:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Den Port Forwarding app gør det muligt at give adgang til systemer på dit lokale netværk fra internettet.';
+$lang['port_forwarding_app_name'] = 'Portvideresendelse';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funktionen understøttes ikke.';
+$lang['port_forwarding_port_forwarding'] = 'Portvideresendelse';
+$lang['port_forwarding_port_range_warning'] = 'Port intervaller skal angives ved hjælp af en enkelt port start (f.eks 15000) til en enkelt port ende (fx 15999).';
