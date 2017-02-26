0a1ad70145b96c54b79e00d22d333d0a7cef68c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:49:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'ポート転送アプリは、インターネットからローカルネットワーク上のシステムへのアクセスを許可することができます。';
+$lang['port_forwarding_app_name'] = 'ポートフォワーディング';
+$lang['port_forwarding_feature_is_not_supported'] = '機能がサポートされていません。';
+$lang['port_forwarding_port_forwarding'] = 'ポートフォワーディング';
+$lang['port_forwarding_port_range_warning'] = 'ポート範囲は、単一のポート端（例えば15999）にシングルポート開始（例えば15000）を使用して指定する必要があります。';
