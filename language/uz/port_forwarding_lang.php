866c7d54c83f1ce7ecb8808f90536cbf968e3ae6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:55:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Ekspeditorlik ilova imkon Internetdan mahalliy tarmoq ustida tizimlari kirish uchun ruxsat beradilar.';
+$lang['port_forwarding_app_name'] = 'Port yo&#39;naltirish';
+$lang['port_forwarding_feature_is_not_supported'] = 'Xususiyati mavjud emas.';
+$lang['port_forwarding_port_forwarding'] = 'Port yo&#39;naltirish';
+$lang['port_forwarding_port_range_warning'] = 'Port tizmalari bir port oxirida (masalan 15999) uchun (masalan, 15000) yagona port boshlang&#39;ich yordamida belgilangan bo&#39;lishi kerak.';
