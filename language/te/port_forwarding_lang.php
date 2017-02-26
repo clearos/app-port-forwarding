a2a01d9639ed8e2562f10004f1d4dda7d2872632
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:54:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'పోర్ట్ ఫార్వార్డింగ్ అనువర్తనం ఇంటర్నెట్ నుండి మీ స్థానిక నెట్వర్క్ వ్యవస్థలు యాక్సెస్ అనుమతించేందుకు సాధ్యం చేస్తుంది.';
+$lang['port_forwarding_app_name'] = 'పోర్ట్ ఫార్వార్డింగ్';
+$lang['port_forwarding_feature_is_not_supported'] = 'ఫీచర్ మద్దతు లేదు.';
+$lang['port_forwarding_port_forwarding'] = 'పోర్ట్ ఫార్వార్డింగ్';
+$lang['port_forwarding_port_range_warning'] = 'పోర్ట్ శ్రేణులు ఒకే పోర్ట్ ఎండ్ (ఉదా 15999) కు ఒకే పోర్ట్ ప్రారంభం ఉపయోగించడం పేర్కొన్న చేయాలి (ఉదా 15000).';
