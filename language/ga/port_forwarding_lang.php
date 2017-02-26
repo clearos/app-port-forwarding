e8b4d2743040fb9d0ecb9e9550036a66fd271a8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:49:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Déanann an app Aghaidh Port sé indéanta rochtain ar chórais chun ligean ar do líonra áitiúil ón Idirlíon.';
+$lang['port_forwarding_app_name'] = 'Port Aghaidh';
+$lang['port_forwarding_feature_is_not_supported'] = 'Níl gné tacaíocht.';
+$lang['port_forwarding_port_forwarding'] = 'Port Aghaidh';
+$lang['port_forwarding_port_range_warning'] = 'Caithfear raonta calafoirt a shonrú ag baint úsáide as tús port amháin (eg 15000) go dtí deireadh port amháin (eg 15999).';
