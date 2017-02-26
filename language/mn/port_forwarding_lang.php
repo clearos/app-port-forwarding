ee8b43efb0e919ce153a6b967dbbd9fc83cf0470
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Порт зуучийн програм боломжтой интернэтээс нь таны локал сүлжээн дээр системд хандах боломж олгодог.';
+$lang['port_forwarding_app_name'] = 'порт зуучийн';
+$lang['port_forwarding_feature_is_not_supported'] = 'Онцлог нь дэмжиж байна.';
+$lang['port_forwarding_port_forwarding'] = 'порт зуучийн';
+$lang['port_forwarding_port_range_warning'] = 'Портын зурвасыг (жишээ нь 15000) нь нэг порт төгсгөл (жишээ нь 15999) нь нэг порт эхлэл ашиглан зааж өгөх ёстой.';
