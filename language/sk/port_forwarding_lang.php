7f48a50246123933e4d396fe8928a44f9e17ecd8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:53:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Forwarding Aplikácia umožňuje povoliť prístup k systémom v lokálnej sieti z Internetu.';
+$lang['port_forwarding_app_name'] = 'port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funkcia nie je podporovaná.';
+$lang['port_forwarding_port_forwarding'] = 'port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Prístavné pásma, musí byť zadaný pomocou tlačidla Štart jediný port (napr 15000) na konci jedného portu (napr 15999).';
