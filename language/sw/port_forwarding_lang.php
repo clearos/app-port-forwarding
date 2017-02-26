974120ea55590185142ae83c7f66adc22bd3b3a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:54:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'programu Port Usambazaji inafanya uwezekano wa kuruhusu upatikanaji wa mifumo ya kwenye mtandao wako ndani kutoka mtandao.';
+$lang['port_forwarding_app_name'] = 'Port Usambazaji';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature si mkono.';
+$lang['port_forwarding_port_forwarding'] = 'Port Usambazaji';
+$lang['port_forwarding_port_range_warning'] = 'kati Port lazima maalum kwa kutumia moja bandari kuanza (kwa mfano 15000) kwa single bandari mwisho (kwa mfano 15999).';
