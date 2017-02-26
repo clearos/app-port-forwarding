ad02999c5364aedf228e6616462e4e92f95b705d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:45:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'التطبيق ميناء الشحن يجعل من الممكن للسماح بالوصول إلى أنظمة على الشبكة المحلية من الإنترنت.';
+$lang['port_forwarding_app_name'] = 'ميناء الشحن';
+$lang['port_forwarding_feature_is_not_supported'] = 'لا يتم اعتماد الميزة.';
+$lang['port_forwarding_port_forwarding'] = 'ميناء الشحن';
+$lang['port_forwarding_port_range_warning'] = 'يجب تحديد نطاقات المنفذ باستخدام بداية منفذ واحد (على سبيل المثال 15000) إلى نهاية ميناء واحد (على سبيل المثال 15999).';
