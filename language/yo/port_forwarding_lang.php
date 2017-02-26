a9a3d5c7b0b1a19d851898a02a57db1d22ca8551
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:56:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The Port Ndari awọn app mu ki o ṣee ṣe lati gba wiwọle si awọn ọna šiše lori rẹ agbegbe nẹtiwọki lati ayelujara.';
+$lang['port_forwarding_app_name'] = 'Port Ndari';
+$lang['port_forwarding_feature_is_not_supported'] = 'Ẹya-ara ti ko ba ni atilẹyin.';
+$lang['port_forwarding_port_forwarding'] = 'Port Ndari';
+$lang['port_forwarding_port_range_warning'] = 'Ibudo awọn sakani gbọdọ wa ni pato nipa lilo a nikan ibudo ibere (eg 15000) to kan nikan ibudo opin (eg 15999).';
