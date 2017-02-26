8ea152645260b6fc710eaed602a6a3575f742594
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'ການ app Port Forwarding ເຮັດໃຫ້ມັນເປັນໄປໄດ້ເພື່ອອະນຸຍາດໃຫ້ເຂົ້າເຖິງລະບົບກ່ຽວກັບການເຄືອຂ່າຍທ້ອງຖິ່ນຂອງທ່ານຈາກອິນເຕີເນັດ.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'ຄຸນນະສົມບັດບໍ່ໄດ້ຖືກສະຫນັບສະຫນູນ.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'ຂອບເຂດ Port ຕ້ອງໄດ້ກໍານົດການນໍາໃຊ້ເປັນການເລີ່ມຕົ້ນທີ່ Port ດຽວ (ຕົວຢ່າງ: 15000) ເພື່ອເປັນທີ່ສຸດທີ່ Port ດຽວ (ຕົວຢ່າງ: 15999).';
