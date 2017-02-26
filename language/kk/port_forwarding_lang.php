4bba84f898d1cbe6fd18b1a3fbd1c737d2419e99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:49:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Forwarding Қолданба Интернеттен жергілікті желідегі жүйелерге қатынасуды рұқсат ету үшін мүмкіндік береді.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Мүмкіндік қолдау көрсетілмейді.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Порт ауқымдары бір порты соңында (мысалы, 15999) бірыңғай порт старт (мысалы 15000) пайдалана отырып, көрсетілген болуы тиіс.';
