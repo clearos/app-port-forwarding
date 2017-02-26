81a796ba77fa027301ec0ba51a058b515ccacc8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:56:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Mae&#39;r app Port Anfon yn ei gwneud yn bosibl i ganiatáu mynediad i systemau ar eich rhwydwaith lleol oddi ar y Rhyngrwyd.';
+$lang['port_forwarding_app_name'] = 'Port Anfon';
+$lang['port_forwarding_feature_is_not_supported'] = 'Nid yw nodwedd yn cael ei gefnogi.';
+$lang['port_forwarding_port_forwarding'] = 'Port Anfon';
+$lang['port_forwarding_port_range_warning'] = 'Rhaid ystodau Port yn cael ei nodi gan ddefnyddio cychwyn borthladd sengl (ee 15000) i ben borthladd sengl (ee 15,999).';
