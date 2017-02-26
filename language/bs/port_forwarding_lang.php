02db50a97aff16e8d8fc0fb725e6e3e7b030434b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:46:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Luka Forwarding aplikacija omogućava da se omogući pristup sistema na lokalnoj mreži sa Interneta.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funkcija nije podržana.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Port kreće mora biti naveden koristeći jedan start port (npr 15000) na jedan port kraju (npr 15999).';
