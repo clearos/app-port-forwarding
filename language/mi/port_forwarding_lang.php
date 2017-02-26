b60ad7696780909365817acfb726cb4eed060fbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'hanga te taupānga Port whakamua e taea ki te tukua te uru ki ngā pūnaha i runga i tou whatunga rohe i te Internet.';
+$lang['port_forwarding_app_name'] = 'Port whakamua';
+$lang['port_forwarding_feature_is_not_supported'] = 'e kore te tautoko āhuatanga.';
+$lang['port_forwarding_port_forwarding'] = 'Port whakamua';
+$lang['port_forwarding_port_range_warning'] = 'Me tohua awhe Port te whakamahi i te tīmatanga tauranga kotahi (hei tauira 15000) ki te mutunga tauranga kotahi (hei tauira 15999).';
