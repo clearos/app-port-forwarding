ebf5d7717f965fcd6cf0f2948e24082cdf710a34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:47:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Porttien App mahdollistaa päästämään järjestelmiin lähiverkossa Internetistä.';
+$lang['port_forwarding_app_name'] = 'Portin uudelleenohjaus';
+$lang['port_forwarding_feature_is_not_supported'] = 'Ominaisuutta ei tueta.';
+$lang['port_forwarding_port_forwarding'] = 'Portin uudelleenohjaus';
+$lang['port_forwarding_port_range_warning'] = 'Porttialueet on määritettävä käyttäen yhtä porttia alku (esim 15000) ja yhden portin pää (esim 15999).';
