c834b41fbcb75d43f692dec4f796525aebf65718
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:52:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Aplicația Port Forwarding face posibilă pentru a permite accesul la sistemele de pe rețeaua locală de pe Internet.';
+$lang['port_forwarding_app_name'] = 'Port forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Caracteristică nu este acceptată.';
+$lang['port_forwarding_port_forwarding'] = 'Port forwarding';
+$lang['port_forwarding_port_range_warning'] = 'intervalele de port trebuie să fie specificate folosind un singur port de pornire (de exemplu, 15000) la un singur capăt de port (de exemplu, 15999).';
