56620efb97ffceed800d56707178cf61e251c752
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'កម្មវិធីកំពង់ផែបញ្ជូនបន្តធ្វើឱ្យវាអាចធ្វើទៅបានដើម្បីអនុញ្ញាតឱ្យចូលដំណើរការទៅប្រព័ន្ធនៅលើបណ្ដាញមូលដ្ឋានរបស់អ្នកពីអ៊ីនធឺណិត។';
+$lang['port_forwarding_app_name'] = 'កំពង់ផែបញ្ជូនបន្ត';
+$lang['port_forwarding_feature_is_not_supported'] = 'លក្ខណៈពិសេសមិនត្រូវបានគាំទ្រ។';
+$lang['port_forwarding_port_forwarding'] = 'កំពង់ផែបញ្ជូនបន្ត';
+$lang['port_forwarding_port_range_warning'] = 'ជួរកំពង់ផែត្រូវតែបានបញ្ជាក់ដោយប្រើការចាប់ផ្តើមកំពង់ផែតែមួយ (ឧ 15000) ទៅច្រកបញ្ចប់តែមួយ (ឧទាហរណ៍ 15999) ។';
