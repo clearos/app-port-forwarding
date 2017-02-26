46d779dd5ba6d326630ff2a61b0574e5157e7751
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Uosto ekspedijavimas programa leidžia suteikti prieigą prie sistemų jūsų vietinio tinklo iš interneto.';
+$lang['port_forwarding_app_name'] = 'ekspedijavimas';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funkcija nepalaikoma.';
+$lang['port_forwarding_port_forwarding'] = 'ekspedijavimas';
+$lang['port_forwarding_port_range_warning'] = 'Uosto diapazonai turi būti nurodomi naudojant vieną uosto pradžią (pvz 15000) į vieną uosto pabaigoje (pvz 15999).';
