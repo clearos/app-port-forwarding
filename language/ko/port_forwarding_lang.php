95a967c2b343b56d1189a00ed9a3b716230c44c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = '포트 포워딩 앱은 가능한 인터넷에서 로컬 네트워크 시스템에 대한 액세스를 허용 할 수 있습니다.';
+$lang['port_forwarding_app_name'] = '포트 포워딩';
+$lang['port_forwarding_feature_is_not_supported'] = '기능은 지원되지 않습니다.';
+$lang['port_forwarding_port_forwarding'] = '포트 포워딩';
+$lang['port_forwarding_port_range_warning'] = '포트 범위는 단일 포트 끝 (예 : 15999)에 (예 : 15000) 단일 포트 시작을 사용하여 지정해야합니다.';
