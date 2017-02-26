a9bf8e94e5c885a8203d8a3f39deaa40d3950d4b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'A Port Forwarding app lehetővé teszi, hogy rendszerekhez való hozzáférés, a helyi hálózaton az interneten.';
+$lang['port_forwarding_app_name'] = 'port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funkció nem támogatott.';
+$lang['port_forwarding_port_forwarding'] = 'port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Porttartományokat kell hozzárendelni egy port start (pl 15000) egyetlen portvég (pl 15999).';
