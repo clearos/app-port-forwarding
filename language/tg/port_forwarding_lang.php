4881ef8eb4d5f74e4e97aa05fc1444ba105795f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:54:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Дар барнома Порт-экспедитор имкон медиҳад, ки дастрасӣ ба системаҳои дар шабакаи маҳаллии шумо аз Интернет.';
+$lang['port_forwarding_app_name'] = 'Порт-экспедитор';
+$lang['port_forwarding_feature_is_not_supported'] = 'Хусусияти дастгирӣ карда намешавад.';
+$lang['port_forwarding_port_forwarding'] = 'Порт-экспедитор';
+$lang['port_forwarding_port_range_warning'] = 'силсилакӯҳҳои Порт бояд истифодаи оғози порти ягона ба ин хотима бандари ягонаи (масалан 15999) зикр карда шаванд (масалан, 15000).';
