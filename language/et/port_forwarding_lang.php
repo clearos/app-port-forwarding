2ec48d904e31c8dbc75719e450a6fd58d6bd488a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:47:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Pordiedastusmenüüsse app võimaldab juurdepääsu võimaldamist süsteemid kohtvõrgu internetist.';
+$lang['port_forwarding_app_name'] = 'pordiedastusega';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funktsiooni ei toetata.';
+$lang['port_forwarding_port_forwarding'] = 'pordiedastusega';
+$lang['port_forwarding_port_range_warning'] = 'Port vahemikke tuleb täpsustada kasutades ühte porti algus (nt 15000) ühe sadama lõpus (nt 15999).';
