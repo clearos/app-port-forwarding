bf8703e5f37df82a3ae0cd7cb788aa1191a76867
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:45:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Forwarding app mümkün İnternet yerli şəbəkə sistemi daxil olmaq üçün edir.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature dəstəklənmir.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Port silsilələri bir port sonunda (məsələn, 15999) (məsələn 15000) bir port start istifadə müəyyən olunmalıdır.';
