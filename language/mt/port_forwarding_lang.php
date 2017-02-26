ddf6b02357be5533ccd9a40cb4f83e84fc2a8077
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Il-app Port Tressiq jagħmilha possibbli sabiex jippermettu aċċess għal sistemi fuq in-netwerk lokali tiegħek mill-Internet.';
+$lang['port_forwarding_app_name'] = 'Tressiq port';
+$lang['port_forwarding_feature_is_not_supported'] = 'Karatteristika ma hija sostnuta.';
+$lang['port_forwarding_port_forwarding'] = 'Tressiq port';
+$lang['port_forwarding_port_range_warning'] = 'firxiet ta &#39;portijiet iridu jiġu speċifikati bl-użu ta bidu port wieħed (eż 15000) għal tmiem f&#39;port wieħed (eż 15999).';
