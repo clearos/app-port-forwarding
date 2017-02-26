b3976e7cfffe61d1c928b03b5ca3261dd3f81126
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:55:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'แอปพลิเคส่งต่อพอร์ตทำให้มันเป็นไปได้เพื่อให้สามารถเข้าถึงระบบเครือข่ายในท้องถิ่นของคุณจากอินเทอร์เน็ต';
+$lang['port_forwarding_app_name'] = 'ส่งต่อพอร์ต';
+$lang['port_forwarding_feature_is_not_supported'] = 'คุณลักษณะที่ไม่สนับสนุน';
+$lang['port_forwarding_port_forwarding'] = 'ส่งต่อพอร์ต';
+$lang['port_forwarding_port_range_warning'] = 'ช่วงของพอร์ตที่ต้องระบุโดยใช้เป็นจุดเริ่มต้นพอร์ตเดียว (เช่น 15000) ไปยังจุดสิ้นสุดพอร์ตเดียว (เช่น 15999)';
