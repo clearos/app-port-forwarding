e05a0c650b60d20bdcec388a7734ff18e0221c6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:46:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'အဆိုပါ port forwarding app ကို Internet မှသင့် local network ပေါ်မှာစနစ်များကိုအသုံးပြုခွင့်ကိုခွင့်ပြုဖို့ကဖြစ်နိုင်ခြေကိုမှန်ကန်စေသည်။';
+$lang['port_forwarding_app_name'] = 'port forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature ကိုထောက်ခံသည်မဟုတ်။';
+$lang['port_forwarding_port_forwarding'] = 'port forwarding';
+$lang['port_forwarding_port_range_warning'] = 'ဆိပ်ကမ်းပ္ပံ port တစ်ခုတည်းအဆုံး (ဥပမာ 15999) မှ port တစ်ခုတည်းစတင် (ဥပမာ 15000) ကို အသုံးပြု. သတ်မှတ်ထားသောရလိမ့်မည်။';
