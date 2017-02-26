5d74d182f1d767796239774fd0d7e259832813eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Forwarding app memungkinkan untuk memungkinkan akses ke sistem pada jaringan lokal Anda dari Internet.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Fitur tidak didukung.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Pelabuhan rentang harus ditentukan dengan menggunakan port start tunggal (misalnya 15000) ke port tunggal akhir (misalnya 15999).';
