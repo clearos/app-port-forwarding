82e58710e387c838a9fad0fba3c66259909ad144
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:55:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Ang Port Pagpapasa app ginagawang posible upang payagan ang access sa mga sistema sa iyong lokal na network mula sa Internet.';
+$lang['port_forwarding_app_name'] = 'port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Tampok ay hindi suportado.';
+$lang['port_forwarding_port_forwarding'] = 'port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Port saklaw ay dapat na tinukoy gamit ang isang solong start port (eg 15000) sa isang solong port end (eg 15999).';
