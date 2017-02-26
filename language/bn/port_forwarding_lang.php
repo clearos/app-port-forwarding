7510df736688946b95fd5ef6edc121db6dded133
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:46:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'পোর্ট ফরওয়ার্ডিং অ্যাপ্লিকেশন এটা সম্ভব ইন্টারনেট থেকে আপনার স্থানীয় নেটওয়ার্কে সিস্টেম অ্যাক্সেস করার জন্য নির্মিত হয়.';
+$lang['port_forwarding_app_name'] = 'পোর্ট ফরওয়ার্ডিং';
+$lang['port_forwarding_feature_is_not_supported'] = 'বৈশিষ্ট্য সমর্থিত নয়.';
+$lang['port_forwarding_port_forwarding'] = 'পোর্ট ফরওয়ার্ডিং';
+$lang['port_forwarding_port_range_warning'] = 'পোর্ট রেঞ্জ (উদাঃ 15000) একটি পোর্ট শেষ (15999 যেমন) থেকে একটি পোর্ট সূচনা ব্যবহার উল্লেখ করা আবশ্যক.';
