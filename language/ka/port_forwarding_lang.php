7a8ed6bb6b3b88ee2280c1ff2c395b0944e07d9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:49:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'პორტის გადამისამართება app შესაძლებელს ხდის დაუშვას სისტემები თქვენს ქსელში ინტერნეტში.';
+$lang['port_forwarding_app_name'] = 'პორტის გადამისამართება';
+$lang['port_forwarding_feature_is_not_supported'] = 'ფუნქცია არ არის მხარდაჭერილი.';
+$lang['port_forwarding_port_forwarding'] = 'პორტის გადამისამართება';
+$lang['port_forwarding_port_range_warning'] = 'Port მერყეობს უნდა მითითებული გამოყენებით ერთი პორტი დაწყება (მაგ 15000) ერთ პორტში ბოლოს (მაგ 15999).';
