11ebcc4c83e44d7aaca120bd21b5cb20214556d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:47:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'La Haveno Forwarding app ebligas permesi aliron al sistemoj en via loka reto de la Interreto.';
+$lang['port_forwarding_app_name'] = 'haveno Ekspedado';
+$lang['port_forwarding_feature_is_not_supported'] = 'Karakterizaĵo ne estas subtenata.';
+$lang['port_forwarding_port_forwarding'] = 'haveno Ekspedado';
+$lang['port_forwarding_port_range_warning'] = 'Haveno intervaloj devas esti precizigitaj uzante sola haveno komenco (ekz 15000) al sola haveno fino (ekz 15999).';
