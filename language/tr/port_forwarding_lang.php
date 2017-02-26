fa0c2e4b73d525bf7beea9403b10e86b806c771b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:55:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Yönlendirme uygulaması mümkün internetten yerel ağınızdaki sistemlere erişim sağlamak için yapar.';
+$lang['port_forwarding_app_name'] = 'port Yönlendirme';
+$lang['port_forwarding_feature_is_not_supported'] = 'Özellik desteklenmiyor.';
+$lang['port_forwarding_port_forwarding'] = 'port Yönlendirme';
+$lang['port_forwarding_port_range_warning'] = 'Liman aralıkları tek bir bağlantı noktası sonuna kadar (örneğin 15.999) (örneğin 15000) tek bir bağlantı noktası başlangıç ​​kullanılarak belirtilmelidir.';
