01579665d971602e95d4b14bf1a7c32dc271d380
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:45:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'App Port Forwarding bën të mundur për të lejuar qasjen në sistemet në rrjetin tuaj lokal nga Interneti.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funksion nuk është i mbështetur.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Shkon port duhet të përcaktohet duke përdorur një fillim të vetme port (p.sh. 15000) në një përfundim të vetëm port (p.sh. 15999).';
