2f0dd0c262d686da28f1ea0213ab4d10fac52e30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'पोर्ट अग्रेषण एप्लिकेशन यह संभव इंटरनेट से अपने स्थानीय नेटवर्क पर प्रणाली के लिए उपयोग की अनुमति देने के लिए बनाता है।';
+$lang['port_forwarding_app_name'] = 'पोर्ट फॉरवार्डिंग';
+$lang['port_forwarding_feature_is_not_supported'] = 'फ़ीचर समर्थित नहीं है।';
+$lang['port_forwarding_port_forwarding'] = 'पोर्ट फॉरवार्डिंग';
+$lang['port_forwarding_port_range_warning'] = 'पोर्ट श्रेणियों (जैसे 15000) एक बंदरगाह अंत (15999 उदाहरण के लिए) के लिए एक बंदरगाह का उपयोग कर प्रारंभ निर्दिष्ट किया जाना चाहिए।';
