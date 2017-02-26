5779abc77b66da909c585b1b9851e5ddacf41726
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:53:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'වරාය යොමු යෙදුම අන්තර්ජාලයේ සිට ඔබේ දේශීය ජාලය මත පද්ධති සඳහා පිවිසීමට ඉඩ දෙනු හැකි වේ.';
+$lang['port_forwarding_app_name'] = 'වරාය පරිවහණ';
+$lang['port_forwarding_feature_is_not_supported'] = 'විශේෂාංගය සහාය නොදක්වයි.';
+$lang['port_forwarding_port_forwarding'] = 'වරාය පරිවහණ';
+$lang['port_forwarding_port_range_warning'] = 'වරාය වැටි තනි වරාය අවසන් (උදා: 15999) වෙත තනි වරාය ආරම්භ (උදා: 15000) භාවිතා කර ගත යුතු ය.';
