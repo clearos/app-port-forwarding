d6ad30ab9795c81c073d23c9111167efaf6f0996
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Portus est ratio de transmissione App permittit loci permittere accessum ad ipsum a lorem.';
+$lang['port_forwarding_app_name'] = 'portum transmittere';
+$lang['port_forwarding_feature_is_not_supported'] = 'Factura non praebetur.';
+$lang['port_forwarding_port_forwarding'] = 'portum transmittere';
+$lang['port_forwarding_port_range_warning'] = 'Portus, Port septa templi, debet specificatur per unum principium (eg (XV)) a porta usque ad finem (puta (XV)CMXCIX).';
