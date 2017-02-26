b6d61b678bfc788ab4a1a8ffc3a50406c4720a31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'पोर्ट अग्रेषित अनुप्रयोग इंटरनेट वरून आपल्या स्थानिक नेटवर्क वर प्रणाली प्रवेश करण्याची परवानगी देण्यासाठी शक्य करते.';
+$lang['port_forwarding_app_name'] = 'पोर्ट अग्रेषित';
+$lang['port_forwarding_feature_is_not_supported'] = 'वैशिष्ट्य समर्थित नाही.';
+$lang['port_forwarding_port_forwarding'] = 'पोर्ट अग्रेषित';
+$lang['port_forwarding_port_range_warning'] = 'पोर्ट श्रेणी एकच पोर्ट प्रारंभ वापरून निर्देशीत करणे आवश्यक आहे (उदा 15000) एकच पोर्ट शेवटी (उदा 15999) पर्यंत.';
