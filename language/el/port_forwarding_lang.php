7633de3f9565e8d64b4d54959f5f9e10e787ec56
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Η εφαρμογή Port Forwarding καθιστά δυνατό να επιτρέψει την πρόσβαση σε συστήματα στο τοπικό σας δίκτυο από το Internet.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Χαρακτηριστικό δεν υποστηρίζεται.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'κυμαίνεται θύρα πρέπει να προσδιορίζεται με τη χρήση ενός ενιαίου έναρξη θύρα (π.χ. 15000) σε ένα ενιαίο τέλος θύρα (π.χ. 15999).';
