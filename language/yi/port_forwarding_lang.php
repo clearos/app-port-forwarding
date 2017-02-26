a6767ac860f39a0e9901143bb052b8ace6ac363d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:56:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'די פּאָרט Forwarding אַפּ מאכט עס מעגלעך צו לאָזן צוטריט צו סיסטעמען אויף אייער היגע נעץ פֿון דער אינטערנעץ.';
+$lang['port_forwarding_app_name'] = 'פּאָרט Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'שטריך איז ניט געשטיצט.';
+$lang['port_forwarding_port_forwarding'] = 'פּאָרט Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'פּאָרט ריינדזשאַז מוזן זיין ספּעסיפיעד ניצן אַ איין פּאָרט אָנהייבן (למשל 15000) צו אַ איין פּאָרט סוף (למשל 15,999).';
