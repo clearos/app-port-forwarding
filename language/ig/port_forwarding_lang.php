a180c47d9cb66118a8b9909e302c772100c5e6d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:49:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The Port ebugharị ngwa na-eme ka o kwe omume na-ekwe ka ohere ka usoro on your local netwọk na Ịntanetị.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Mma a na-anaghị akwado.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Port ranges ga-kpọmkwem site na iji otu ọdụ ụgbọ mmiri mmalite (eg 15000) na otu ọdụ ụgbọ mmiri ọgwụgwụ (eg 15999).';
