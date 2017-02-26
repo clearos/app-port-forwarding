0494846b5caa7409f103852c465f75fb82cad33f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:55:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Các ứng dụng Port Forwarding làm cho nó có thể cho phép truy cập vào hệ thống mạng nội bộ của bạn từ Internet.';
+$lang['port_forwarding_app_name'] = 'Cổng chuyển tiếp';
+$lang['port_forwarding_feature_is_not_supported'] = 'Tính năng không được hỗ trợ.';
+$lang['port_forwarding_port_forwarding'] = 'Cổng chuyển tiếp';
+$lang['port_forwarding_port_range_warning'] = 'dải cổng phải được xác định bằng một sự khởi đầu cổng duy nhất (ví dụ 15000) đến một cổng kết thúc duy nhất (ví dụ 15.999).';
