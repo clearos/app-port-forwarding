2b1a54bc7a8e64880f30f914fd8dfeda0fe929aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:51:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Апликацијата на порт пренасочување го прави возможно да се овозможи пристап до системи во вашата локална мрежа од интернет.';
+$lang['port_forwarding_app_name'] = 'порт пренасочување';
+$lang['port_forwarding_feature_is_not_supported'] = 'Функција не е поддржана.';
+$lang['port_forwarding_port_forwarding'] = 'порт пренасочување';
+$lang['port_forwarding_port_range_warning'] = 'Порт движи мора да се наведе со користење на еден почеток порта (на пр 15000) на еден крај порта (на пример, 15999).';
