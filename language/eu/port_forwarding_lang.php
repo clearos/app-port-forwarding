234a77f01413e6d0d6a22e82ab3a62e130f3cab2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:45:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Port Forwarding aplikazioa da, eta aukera zure Internetetik sare lokaleko sistemak sarbidea baimendu da.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature ez da onartzen.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Port areak zehaztu behar ataka Irteeran bakar bat erabiliz (adibidez, 15000) bakar bat portu amaieran (adibidez, 15999) ra.';
