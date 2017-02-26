d58ef42a331fb735cb20b592540f664544c8fa7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:47:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'L&#39;application Port Forwarding permet d&#39;autoriser l&#39;accès à des systèmes sur votre réseau local à partir d&#39;Internet.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Feature est pas pris en charge.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'les plages de ports doivent être spécifiés en utilisant un démarrage à port unique (par exemple 15000) à une extrémité d&#39;un seul port (par exemple, 15999).';
