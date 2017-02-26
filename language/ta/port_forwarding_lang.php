25fe4582f33319dd2bb76ba809b19d36e9e6f0a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:54:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'துறைமுக பகிர்தல் பயன்பாட்டை அது சாத்தியம் இணையத்தில் இருந்து உங்கள் உள்ளூர் பிணைய அமைப்புகள் அணுகலை அனுமதிக்க செய்கிறது.';
+$lang['port_forwarding_app_name'] = 'துறைமுக பகிர்தல்';
+$lang['port_forwarding_feature_is_not_supported'] = 'அம்சம் ஆதரவளிக்கவில்லை.';
+$lang['port_forwarding_port_forwarding'] = 'துறைமுக பகிர்தல்';
+$lang['port_forwarding_port_range_warning'] = 'போர்ட் எல்லைகள், இது ஒற்றை போர்ட்டை இறுதியில் (எ.கா. 15999) ஒரு ஒற்றை போர்ட்டை தொடக்கத்தில் பயன்படுத்தி குறிப்பிடப்பட வேண்டும் (எ.கா. 15000).';
