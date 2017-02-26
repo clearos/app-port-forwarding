c6d66bed4c8d3c1afb749eed37efbfbb340c583f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:56:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Uhlelo lokusebenza Port Ukudlulisela kwenza kube nokwenzeka ukuba uvumele ukufinyelela izinhlelo kunethiwekhi yakho bendawo Internet.';
+$lang['port_forwarding_app_name'] = 'Port Ukudlulisela';
+$lang['port_forwarding_feature_is_not_supported'] = 'Isici ayisekelwe.';
+$lang['port_forwarding_port_forwarding'] = 'Port Ukudlulisela';
+$lang['port_forwarding_port_range_warning'] = 'Port amabanga kumele icaciswe usebenzisa port isiqalo olulodwa (isib 15000) ukuze port ekupheleni olulodwa (isib 15999).';
