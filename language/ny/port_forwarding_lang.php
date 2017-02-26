190717fb1d6539bb3c919b7753ac6e4f3d1eca4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:46:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The app Port kutumiza limachititsa kulola kupeza kachitidwe pa Intaneti kwanuko pa Intaneti.';
+$lang['port_forwarding_app_name'] = 'Port kutumiza';
+$lang['port_forwarding_feature_is_not_supported'] = 'Mbali sagwirizana.';
+$lang['port_forwarding_port_forwarding'] = 'Port kutumiza';
+$lang['port_forwarding_port_range_warning'] = 'ranges Port ayenera mwachindunji kugwiritsa ntchito limodzi doko chiyambi (mwachitsanzo 15000) kuti limodzi doko mapeto (mwachitsanzo 15999).';
