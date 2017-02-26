dcd09efa80a5641d88d59f7447d09460a82a0429
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'App a Port èkspédision fè li posib yo ki pèmèt aksè nan sistèm sou rezo lokal ou a nan entènèt la.';
+$lang['port_forwarding_app_name'] = 'Port èkspédision';
+$lang['port_forwarding_feature_is_not_supported'] = 'Karakteristik pa sipòte.';
+$lang['port_forwarding_port_forwarding'] = 'Port èkspédision';
+$lang['port_forwarding_port_range_warning'] = 'chenn Port yo dwe espesifye lè l sèvi avèk yon sèl pò kòmanse (egzanp 15000) nan yon sèl fen pò (egzanp 15999).';
