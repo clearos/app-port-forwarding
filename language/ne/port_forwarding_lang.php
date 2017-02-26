e8ae96547d1afdd24bc8b94b55035260ebfafb1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:52:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'पोर्ट फरवार्ड अनुप्रयोग सम्भव इन्टरनेटमा तपाईंको स्थानीय नेटवर्क मा प्रणाली पहुँच अनुमति बनाउँछ।';
+$lang['port_forwarding_app_name'] = 'पोर्ट फरवार्ड';
+$lang['port_forwarding_feature_is_not_supported'] = 'सुविधा समर्थित छैन।';
+$lang['port_forwarding_port_forwarding'] = 'पोर्ट फरवार्ड';
+$lang['port_forwarding_port_range_warning'] = 'पोर्ट दायरा एकल पोर्ट सुरु प्रयोग निर्दिष्ट हुनु पर्छ (जस्तै 15000) एकल पोर्ट अन्त (जस्तै 15999) गर्न।';
