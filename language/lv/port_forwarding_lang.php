c04a4f05dca474c733a2694ab2d277ee6b530308
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:50:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Ostas Forwarding app ļauj atļaut piekļuvi sistēmām lokālajā tīklā no interneta.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Funkcija netiek atbalstīta.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Ostu diapazoni ir jānorāda, izmantojot vienu portu sākums (piem 15000) uz vienu ostu beigām (piemēram, 15999).';
