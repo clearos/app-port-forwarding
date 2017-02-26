0074f3868d25936ed3ba9403c4e7b84d09b2fd31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:52:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'ਪੋਰ੍ਟ ਫਾਰਵਰਡਿੰਗ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਇਹ ਸੰਭਵ ਇੰਟਰਨੈੱਟ ਤੁਹਾਡੇ ਲੋਕਲ ਨੈੱਟਵਰਕ ਤੇ ਸਿਸਟਮ ਲਈ ਪਹੁੰਚ ਮਨਜੂਰ ਕਰਨ ਲਈ ਕਰਦਾ ਹੈ.';
+$lang['port_forwarding_app_name'] = 'ਪੋਰ੍ਟ ਫਾਰਵਰਡਿੰਗ';
+$lang['port_forwarding_feature_is_not_supported'] = 'ਵਿਸ਼ੇਸ਼ਤਾ ਸਹਿਯੋਗ ਨਹੀ ਹੈ.';
+$lang['port_forwarding_port_forwarding'] = 'ਪੋਰ੍ਟ ਫਾਰਵਰਡਿੰਗ';
+$lang['port_forwarding_port_range_warning'] = 'ਪੋਰ੍ਟ ਰੇਜ਼ (ਉਦਾਹਰਨ ਲਈ 15000) ਨੂੰ ਇੱਕ ਸਿੰਗਲ ਪੋਰਟ ਅੰਤ (15999 ਉਦਾਹਰਨ) ਨੂੰ ਇੱਕ ਸਿੰਗਲ ਪੋਰਟ ਸ਼ੁਰੂ ਵਰਤ ਨਿਰਧਾਰਤ ਕੀਤਾ ਜਾਣਾ ਚਾਹੀਦਾ ਹੈ.';
