e4800ac9236aff33d615a74ddc307a19a4ec138c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:46:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'Luka Forwarding app omogućuje da omogući pristup sustavima na lokalnoj mreži s Interneta.';
+$lang['port_forwarding_app_name'] = 'Port Forwarding';
+$lang['port_forwarding_feature_is_not_supported'] = 'Značajka nije podržana.';
+$lang['port_forwarding_port_forwarding'] = 'Port Forwarding';
+$lang['port_forwarding_port_range_warning'] = 'Lučkih područja mora biti naveden koristeći jedan port start (npr 15000) na jednom kraju luke (npr 15999).';
