aa5260c17b9dc3848d83003180a37a6fad347007
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:49:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The Port Áframsending app sem gerir það mögulegt að leyfa aðgang að kerfum á staðarnetinu af Netinu.';
+$lang['port_forwarding_app_name'] = 'Port Áframsending';
+$lang['port_forwarding_feature_is_not_supported'] = 'Eiginleiki er ekki studd.';
+$lang['port_forwarding_port_forwarding'] = 'Port Áframsending';
+$lang['port_forwarding_port_range_warning'] = 'Höfn svið skal tilgreint með einum höfn byrjun (td 15000) við eina gátt enda (td 15999).';
