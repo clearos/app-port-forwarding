e3758a5438254cc36707ea0e401228e4c44cb56a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:52:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'برنامه بندر حمل و نقل را ممکن می سازد برای اجازه دسترسی به سیستم در شبکه محلی خود را از اینترنت می باشد.';
+$lang['port_forwarding_app_name'] = 'پورت حمل و نقل';
+$lang['port_forwarding_feature_is_not_supported'] = 'ویژگی پشتیبانی نمی شود.';
+$lang['port_forwarding_port_forwarding'] = 'پورت حمل و نقل';
+$lang['port_forwarding_port_range_warning'] = 'محدوده پورت باید با استفاده از یک پورت شروع تنها به یک پایان پورت (به عنوان مثال 15999) مشخص شود (به عنوان مثال 15000).';
