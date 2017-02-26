a01ffa43beb9244848ed5e3888ec603ade0db35d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:48:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'પોર્ટ આગળ ધપાવવાનું એપ્લિકેશન તે શક્ય ઇન્ટરનેટ પરથી તમારા સ્થાનિક નેટવર્ક પર સિસ્ટમો માટે પ્રવેશને પરવાનગી આપવા માટે બનાવે છે.';
+$lang['port_forwarding_app_name'] = 'પોર્ટ આગળ ધપાવવાનું';
+$lang['port_forwarding_feature_is_not_supported'] = 'લક્ષણ આધારભૂત નથી.';
+$lang['port_forwarding_port_forwarding'] = 'પોર્ટ આગળ ધપાવવાનું';
+$lang['port_forwarding_port_range_warning'] = 'પોર્ટ રેન્જ (દા.ત. 15000) એક બંદર ઓવરને (15999 દા.ત.) માટે એક બંદર શરૂઆત મદદથી સ્પષ્ટ થયેલ હોવું જ જોઈએ.';
