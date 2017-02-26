f00269997ca900abd5c303e339e7c425f19a0efe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 22:45:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/port_forwarding_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['port_forwarding_app_description'] = 'The Port վերահասցեավորում ծրագիրը հնարավոր է դարձնում թույլ տալ մուտք գործել համակարգերի ձեր տեղական ցանցի Ինտերնետում:';
+$lang['port_forwarding_app_name'] = 'Պորտ վերահասցեավորում';
+$lang['port_forwarding_feature_is_not_supported'] = 'Խաղարկային չի ապահովվում:';
+$lang['port_forwarding_port_forwarding'] = 'Պորտ վերահասցեավորում';
+$lang['port_forwarding_port_range_warning'] = 'Port միջակայքերը պետք է ձեւավորել օգտագործելով մի նավահանգիստ մեկնարկ (օրինակ 15000) է մեկ նավահանգիստ վերջում (օրինակ 15999):';
